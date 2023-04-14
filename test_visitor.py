18d0717b56ae1e66ec05c9b0cfb26eeccb2d1dfb
Ryan Mark <ma_xingmin@163.com>
2023-04-14 11:33:41 +0800

    🦄 refactor(test_visitor): add VisitorMixin

--- test_visitor.py
+++ test_visitor.py
@@ -7,7 +7,26 @@ from astxml import AstXml
 from visitor import LineLengthVisitor
 
 
-class LineLengthVisitorTest(TestCase):
+class VisitorMixin:
+    visitor_type = None
+
+    def run_visitor(self, code: str, xml_filename: str = None):
+        self.assertIsNotNone(self.visitor_type, "Visitor type not defined.")
+
+        self.ctx = AnalysisContext("test.py")
+        visitor = LineLengthVisitor(self.ctx)
+        ast_node = ast.parse(code)
+        visitor.visit(ast_node)
+
+        if xml_filename:
+            AstXml(ast_node).save_file("dump/" + xml_filename)
+
+    # def assert_found_issue(self, lineno:int, code: str):
+
+
+class LineLengthVisitorTest(TestCase, VisitorMixin):
+    visitor_type = LineLengthVisitor
+
     def test_visit(self):
         code = """
 print('short line')
@@ -15,16 +34,11 @@ print('this is a very, very, very, very, very, very, very, very, very, very, ver
 print('this is a very, very, very, very, very, very, very, very, very, very, very, very long line...')
         """.strip()
 
-        ctx = AnalysisContext("test.py")
-        visitor = LineLengthVisitor(ctx)
-        ast_node = ast.parse(code)
-        visitor.visit(ast_node)
+        self.run_visitor(code, xml_filename="line-length.xml")
 
-        AstXml(ast_node).save_file("dump/line-length.xml")
+        self.assertEqual(2, len(self.ctx.issues))
 
-        self.assertEqual(2, len(ctx.issues))
-
-        issue = ctx.issues[0]
+        issue = self.ctx.issues[0]
         self.assertEqual((2, "W0001"), (issue.line, issue.code))
 
     def test_visit_docstring(self):
@@ -37,15 +51,9 @@ def fn():
     pass        
         """.strip()
 
-        ctx = AnalysisContext("test.py")
-        visitor = LineLengthVisitor(ctx)
+        self.run_visitor(code, xml_filename="doc-string-length.xml")
 
-        ast_node = ast.parse(code)
-        visitor.visit(ast_node)
+        self.assertEqual(1, len(self.ctx.issues))
 
-        AstXml(ast_node).save_file("dump/doc-string-length.xml")
-
-        self.assertEqual(1, len(ctx.issues))
-
-        issue = ctx.issues[0]
+        issue = self.ctx.issues[0]
         self.assertEqual((3, "W0001"), (issue.line, issue.code))
