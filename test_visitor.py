5d068e3ffc95044fe9cb59f839b1fa81298d5fd2
Ryan Mark <ma_xingmin@163.com>
2023-04-14 11:16:33 +0800

    ✨ feat(visitor): add docstring length check

--- test_visitor.py
+++ test_visitor.py
@@ -12,6 +12,7 @@ class LineLengthVisitorTest(TestCase):
         code = """
 print('short line')
 print('this is a very, very, very, very, very, very, very, very, very, very, very, very long line...')
+print('this is a very, very, very, very, very, very, very, very, very, very, very, very long line...')
         """.strip()
 
         ctx = AnalysisContext("test.py")
@@ -21,7 +22,30 @@ print('this is a very, very, very, very, very, very, very, very, very, very, ver
 
         AstXml(ast_node).save_file("dump/line-length.xml")
 
-        self.assertEqual(1, len(ctx.issues))
-        issue = ctx.issues[0]
+        self.assertEqual(2, len(ctx.issues))
 
+        issue = ctx.issues[0]
         self.assertEqual((2, "W0001"), (issue.line, issue.code))
+
+    def test_visit_docstring(self):
+        code = """
+def fn():
+    '''
+    This is a very, very, very, very, very, very, very, very, very, very, very, very long doc string
+    The second line
+    '''
+    pass        
+        """.strip()
+
+        ctx = AnalysisContext("test.py")
+        visitor = LineLengthVisitor(ctx)
+
+        ast_node = ast.parse(code)
+        visitor.visit(ast_node)
+
+        AstXml(ast_node).save_file("dump/doc-string-length.xml")
+
+        self.assertEqual(1, len(ctx.issues))
+
+        issue = ctx.issues[0]
+        self.assertEqual((3, "W0001"), (issue.line, issue.code))
--- visitor.py
+++ visitor.py
@@ -4,12 +4,16 @@ import ast
 
 class LineLengthVisitor(ast.NodeVisitor):
     max_length = 79
+    docstring_max_length = 72
 
     def __init__(self, ctx: AnalysisContext):
         super().__init__()
         self.ctx = ctx
 
     def visit(self, node: ast.AST):
+        if isinstance(node, (ast.AsyncFunctionDef, ast.FunctionDef, ast.ClassDef)):
+            self.check_docstring(node)
+
         if (
             "end_col_offset" in node._attributes
             and node.end_col_offset >= self.max_length
@@ -18,4 +22,37 @@ class LineLengthVisitor(ast.NodeVisitor):
                 node, "W0001", f"Exceed max line length({self.max_length})"
             )
         else:
+            # exit current ast node, visit next node
             self.generic_visit(node)
+
+    def check_docstring(self, node: ast.AST):
+        docstring_node = self.get_docstring_node(node)
+        if not docstring_node:
+            return self.generic_visit(node)
+
+        for offset, line in enumerate(docstring_node.value.split("\n")):
+            if len(line) > self.docstring_max_length:
+                lineno = docstring_node.lineno + offset
+                self.ctx.add_issue(
+                    node,
+                    "W0001",
+                    f"Docstring for {node.name} exceed max length({self.docstring_max_length})",
+                    lineno=lineno,
+                )
+
+    def get_docstring_node(self, node: ast.AST):
+        if not isinstance(
+            node, (ast.AsyncFunctionDef, ast.FunctionDef, ast.ClassDef, ast.Module)
+        ):
+            raise TypeError("%r can't have docstrings" % node.__class__.__name__)
+
+        if not (node.body and isinstance(node.body[0], ast.Expr)):
+            return None
+
+        node = node.body[0].value
+        if isinstance(node, ast.Str):
+            return node
+        elif isinstance(node, ast.Constant) and isinstance(node.value, str):
+            return node
+        else:
+            return None
